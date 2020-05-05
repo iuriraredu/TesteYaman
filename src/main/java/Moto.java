8a08de070d23df9b01fc4cafc72c409807a4b4ba
IuriRaRedu <iuri.ramos.reducino@gmail.com>
2020-05-05 20:30:15 -0300

    Atualização do código

--- src/main/java/Moto.java
+++ src/main/java/Moto.java
@@ -8,27 +8,21 @@ public class Moto {
     public void setCor(String cor) {
         Cor = cor;
     }
-
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
-
     public void setFabricante(String fabricante) {
         this.fabricante = fabricante;
     }
-
     public void setCilindradas(int cilindradas) {
         Cilindradas = cilindradas;
     }
-
     public void setQtdMarchas(int qtdMarchas) {
         this.qtdMarchas = qtdMarchas;
     }
-
     public void setAno(int ano) {
         this.ano = ano;
     }
-
     public void setAutomatica(boolean automatica) {
         this.automatica = automatica;
     }
@@ -37,29 +31,22 @@ public class Moto {
     public String getCor() {
         return Cor;
     }
-
     public String getModelo() {
         return modelo;
     }
-
     public String getFabricante() {
         return fabricante;
     }
-
     public int getCilindradas() {
         return Cilindradas;
     }
-
     public int getQtdMarchas() {
         return qtdMarchas;
     }
-
     public int getAno() {
         return ano;
     }
-
     public boolean getAutomatica() {
         return automatica;
     }
-
 }
