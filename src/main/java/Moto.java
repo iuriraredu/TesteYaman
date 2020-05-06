676c9b61b4327a5a6754ed1980dfd65dc892018e
IuriRaRedu <iuri.ramos.reducino@gmail.com>
2020-05-06 00:44:17 -0300

    Atualização do código

--- src/main/java/Moto.java
+++ src/main/java/Moto.java
@@ -1,21 +1,45 @@
 public class Moto {
 
-    private String Cor, modelo, fabricante;
-    private int Cilindradas, qtdMarchas, ano;
+    private String cor, fabricante;
+    private int cilindradas, qtdMarchas, ano;
     private boolean automatica;
+    private double velocidadeAtual, velocidadeMaxima;
+
+    //get
+    public String getCor() {
+        return cor;
+    }
+    public String getFabricante() {
+        return fabricante;
+    }
+    public int getCilindradas() {
+        return cilindradas;
+    }
+    public int getQtdMarchas() {
+        return qtdMarchas;
+    }
+    public int getAno() {
+        return ano;
+    }
+    public boolean getAutomatica() {
+        return automatica;
+    }
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+    public double getVelocidadeAtual() {
+        return velocidadeAtual;
+    }
 
     //set
     public void setCor(String cor) {
-        Cor = cor;
-    }
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
+        this.cor = cor;
     }
     public void setFabricante(String fabricante) {
         this.fabricante = fabricante;
     }
     public void setCilindradas(int cilindradas) {
-        Cilindradas = cilindradas;
+        this.cilindradas = cilindradas;
     }
     public void setQtdMarchas(int qtdMarchas) {
         this.qtdMarchas = qtdMarchas;
@@ -27,26 +51,29 @@ public class Moto {
         this.automatica = automatica;
     }
 
-    //get
-    public String getCor() {
-        return Cor;
+    //construtor
+    public Moto (String cor,
+                 String fabricante,
+                 int cilindradas,
+                 int qtdMarchas,
+                 int ano,
+                 boolean automatica,
+                 double velocidadeMaxima){
+        this.cor = cor;
+        this.fabricante = fabricante;
+        this.cilindradas = cilindradas;
+        this.qtdMarchas = qtdMarchas;
+        this.ano = ano;
+        this.automatica = automatica;
+        this.velocidadeMaxima = velocidadeMaxima;
+        velocidadeAtual = 0;
     }
-    public String getModelo() {
-        return modelo;
+
+    // ​acelerar e parar
+    public void acelerar(double qtd){
+
     }
-    public String getFabricante() {
-        return fabricante;
-    }
-    public int getCilindradas() {
-        return Cilindradas;
-    }
-    public int getQtdMarchas() {
-        return qtdMarchas;
-    }
-    public int getAno() {
-        return ano;
-    }
-    public boolean getAutomatica() {
-        return automatica;
+    public void parar(){
+
     }
 }
--- src/main/java/TesteMoto.java
+++ src/main/java/TesteMoto.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class TesteMoto {
+    private static Moto bis;
+
+    @BeforeAll
+    public static void iniciaTudo(){
+        bis = new Moto("Preta","Honda",2,4,2020,false,100);
+    }
+
+
 }
